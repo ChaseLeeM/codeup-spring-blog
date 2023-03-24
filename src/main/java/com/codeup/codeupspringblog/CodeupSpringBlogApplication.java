87330ea24fff2aab6be30f0d46c525ff7d59c811
Chase Medford <chasemedford1@gmail.com>
2023-03-24 16:53:36 -0500

    First Exercise completed.

--- src/main/java/com/codeup/codeupspringblog/CodeupSpringBlogApplication.java
+++ src/main/java/com/codeup/codeupspringblog/CodeupSpringBlogApplication.java
@@ -13,4 +13,6 @@ public class CodeupSpringBlogApplication {
 	}
 
 
+
+
 }
---
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -0,0 +1,32 @@
+package com.codeup.codeupspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PostController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String getPosts() {
+        return "View all posts";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String getPost(@PathVariable Long id) {
+        return "View post with id: " + id;
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String createPostForm() {
+        return "View the form for creating a post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String createPost() {
+        return "Create a new post";
+    }
+}
